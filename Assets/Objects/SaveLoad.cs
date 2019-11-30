cbb1f68acc4fde81e48dfc2e40e8a0b68b86740c
YdeaGames <ydeagames@gmail.com>
2019-12-01 04:41:34 +0900

    タイルデータ読み込み

---
+++ Assets/Objects/SaveLoad.cs
@@ -0,0 +1,196 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Text;
+
+// Super easy saving/loading of binary data for Unity games
+// by @kkukshtel
+// License: Do whatever!
+// https://gist.github.com/kkukshtel/ebaff3953d6db3567b81652bc55101b2
+
+public enum SaveType
+{
+    //Add more types here
+    Map,
+    Match,
+    Options
+}
+public static class SaveLoad
+{
+    public delegate T DataReader<T>(FileStream fs)
+        where T : class;
+    public delegate void DataWriter<T>(FileStream fs, T data)
+        where T : class;
+
+    // Make more properites like this to easily access file names
+    public static List<string> Maps
+    {
+        get { return GetFilesOfType(SaveType.Map); }
+    }
+    
+    //Points to User/AppData/LocalLow/DefaultCompany/YourGame/...
+    public static string dataPath = Application.persistentDataPath;
+
+    // Add other directories here to indicate where you want to save
+    public static Dictionary<SaveType, string> gameDirectories = new Dictionary<SaveType, string>
+    {
+        {SaveType.Map, dataPath + "/maps/"},
+        {SaveType.Match, dataPath + "/matches/"},
+        {SaveType.Options, dataPath + "/options/"},
+    };
+    
+     // Add your file extensions here - they can be anything!
+    public static Dictionary<SaveType, string> gameFileExtensions = new Dictionary<SaveType, string>
+    {
+        {SaveType.Map, ".map"},
+        {SaveType.Match, ".match"},
+        {SaveType.Options, ".options"},
+    };
+
+    // Call this at the start of your game if you want unity to make your directories for you to ensure they are there
+    public static void CreateGameDirectories()
+    {
+        foreach (KeyValuePair<SaveType, string> directory in gameDirectories)
+        {
+            Directory.CreateDirectory(directory.Value);
+        }
+    }
+
+    //Call this to save the data of your game. highly recommend using JsonUtility.ToJson to populate the data variable
+    public static void SaveData(string data, string nameOfFile, SaveType typeOfFile, bool overwrite = false)
+    {
+        string file = FileName(nameOfFile, typeOfFile);
+
+        try
+        {
+            //overwrite protection
+            if (!overwrite && File.Exists(file))
+            {
+                Debug.Log(file + " already exists.");
+                return;
+            }
+
+            Debug.Log("writing " + file);
+
+            // create the file
+            using (FileStream fs = File.Create(file))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes(data);
+                fs.Write(info, 0, info.Length);
+            }
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    public static void SaveBinary<T>(T data, string nameOfFile, SaveType typeOfFile, DataWriter<T> dataWriter, bool overwrite = false)
+        where T : class
+    {
+        if (data == null)
+            return;
+
+        string file = FileName(nameOfFile, typeOfFile);
+
+        try
+        {
+            //overwrite protection
+            if (!overwrite && File.Exists(file))
+            {
+                Debug.Log(file + " already exists.");
+                return;
+            }
+
+            Debug.Log("writing " + file);
+
+            // create the file
+            using (FileStream fs = File.Create(file))
+            {
+                dataWriter(fs, data);
+            }
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    // Call this to load the data of your game. highly recommend wrapping this call with JSON utility like so:
+    // YourSerializeableClass loadedObject = JsonUtility.FromJson<YourSerializeableClass>(SaveLoad.LoadData(nameOfThingToLoad, SaveType.Map));
+    public static string LoadData(string nameOfFile, SaveType typeOfFile)
+    {
+        string file = FileName(nameOfFile, typeOfFile);
+        string data = "";
+        try
+        {
+            //make sure file exists
+            if (File.Exists(file))
+            {
+                Debug.Log("loading : " + file);
+                using (StreamReader sr = File.OpenText(file))
+                {
+                    data = sr.ReadToEnd();
+                    return data;
+                }
+            }
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+        return null;
+    }
+
+    public static T LoadBinary<T>(string nameOfFile, SaveType typeOfFile, DataReader<T> dataReader)
+        where T: class
+    {
+        string file = FileName(nameOfFile, typeOfFile);
+        try
+        {
+            //make sure file exists
+            if (File.Exists(file))
+            {
+                Debug.Log("loading : " + file);
+                using (FileStream sr = File.OpenRead(file))
+                {
+                    return dataReader(sr);
+                }
+            }
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+        return null;
+    }
+
+    //Gets the full directory filename for a given filename
+    static string FileName(string fileName, SaveType fileType)
+    {
+        return gameDirectories[fileType] + fileName + gameFileExtensions[fileType];
+    }
+    
+    //Get all files in your save directory of file type
+    //great for filling out menus
+    public static List<string> GetFilesOfType(SaveType fileType)
+    {
+        Debug.Log($"gettting files at {gameDirectories[fileType]} of extension type {gameFileExtensions[fileType]}");
+        DirectoryInfo dir = new DirectoryInfo(gameDirectories[fileType]);
+        FileInfo[] info = dir.GetFiles("*" + gameFileExtensions[fileType]);
+        List<string> fileNames = new List<string>();
+        foreach (FileInfo f in info) 
+        { 
+            fileNames.Add(Path.GetFileNameWithoutExtension(f.FullName)); 
+        }
+        return fileNames;
+    }
+}
+
---
+++ Assets/Objects/TileStage.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using System.IO;
+
+public class TileStage : MonoBehaviour
+{
+    public class StageData
+    {
+        // 4 bytes マジックバイト 0xbebedead
+        // 4 bytes width:int height:int
+        // width*height bytes マップデータ
+
+        public static readonly byte[] MagicByte = { 0xbe, 0xbe, 0xde, 0xad };
+
+        public struct MapData
+        {
+            public short width, height;
+            public byte[] data;
+        }
+
+        public MapData data;
+
+        public bool Valid
+        {
+            get
+            {
+                return data.data != null;
+            }
+        }
+
+        public void SaveMap(FileStream fs)
+        {
+            {
+                fs.Write(MagicByte, 0, MagicByte.Length);
+            }
+            {
+                fs.Write(BitConverter.GetBytes(data.width), 0, 2);
+                fs.Write(BitConverter.GetBytes(data.height), 0, 2);
+            }
+            {
+                fs.Write(data.data, 0, data.data.Length);
+            }
+        }
+
+        public bool LoadMap(FileStream fs)
+        {
+            data = default;
+            MapData map;
+
+            {
+                int cap = 4;
+                var mgbcheck = new byte[cap];
+                if (fs.Read(mgbcheck, 0, cap) < cap)
+                    return false;
+                if (!MagicByte.SequenceEqual(mgbcheck))
+                    return false;
+            }
+
+            {
+                int cap = 4;
+                byte[] size = new byte[cap];
+                if (fs.Read(size, 0, cap) < cap)
+                    return false;
+                short width = BitConverter.ToInt16(size, 0);
+                short height = BitConverter.ToInt16(size, 2);
+                if (width < 0)
+                    return false;
+                if (height < 0)
+                    return false;
+                map.width = width;
+                map.height = height;
+            }
+
+            {
+                int cap = (int)(map.width * map.height);
+                byte[] data = new byte[cap];
+                if (fs.Read(data, 0, cap) < cap)
+                    return false;
+                map.data = data;
+            }
+
+            data = map;
+            return true;
+        }
+    }
+
+    public Prefabs mapTiles;
+    public string mapName = "sample";
+    public StageData map;
+
+    public void SaveMap()
+    {
+        map = new StageData();
+        map.data.width = 100;
+        map.data.height = 100;
+        map.data.data = new byte[100*100];
+        SaveLoad.SaveBinary(map, mapName, SaveType.Map, (fs, data) => data.SaveMap(fs), overwrite: true);
+    }
+
+    public void LoadMap()
+    {
+        map = SaveLoad.LoadBinary(mapName, SaveType.Map, fs =>
+        {
+            var data = new StageData();
+            if (data.LoadMap(fs))
+                return data;
+            return null;
+        });
+        foreach (Transform child in transform)
+            Destroy(child.gameObject);
+        if (map != null)
+            for (int iy = 0; iy < map.data.height; iy++)
+                for (int ix = 0; ix < map.data.width; ix++)
+                {
+                    var id = map.data.data[ix + iy * map.data.width];
+                    var obj = Instantiate(mapTiles.prefabs[id], transform);
+                    obj.transform.localPosition = new Vector3(ix, 0, iy);
+                }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SaveLoad.CreateGameDirectories();
+
+        LoadMap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
