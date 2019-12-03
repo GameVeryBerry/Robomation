452ba862e01378ca1ba358fa32aa001854f31433
ogwtks-tzzo <mathutakeshi@gmail.com>
2019-12-03 20:31:50 +0900

    ゴールパネルが設置できるようになった

--- Assets/Objects/Delivery/Delivery.cs
+++ Assets/Objects/Delivery/Delivery.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Delivery : MonoBehaviour
+public class Delivery : MonoBehaviour, ITile
 {
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,7 @@ public class Delivery : MonoBehaviour
     {
         
     }
-    public void OnTileAction()
+    public void OnTileAction(RobotController robot)
     {
         SceneManager.LoadScene("ResultScene");
     }
