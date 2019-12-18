7bc019aaecbcb5652b6585c58cbe7bc737d06286
Seraphim <seraphim@soul-magus.com>
2019-12-18 20:14:55 +0900

    Added Power Generation mode on ElectricController

--- Assets/Objects/Electrics/ElectricController.cs
+++ Assets/Objects/Electrics/ElectricController.cs
@@ -1,9 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ElectricController : MonoBehaviour
 {
+    // モードステートインターフェース
+    public interface IMode
+    {
+        // 毎ロボットチックの処理 (今は1秒ごと)
+        void OnTick(RobotController robot);
+    }
+    // 発電モード
+    public class GenerateMode : IMode
+    {
+        void IMode.OnTick(RobotController robot)
+        {
+            throw new NotImplementedException();
+        }
+
+        void OnTick(RobotController robot)
+        {
+            // ～～～～～
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
